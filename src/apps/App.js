0e6034ee07ba902bf6cd9d33749da573fea28d3f
Mohit <mohitgadhavi1@gmail.com>
2021-11-18 09:28:05 +0530

    regular commit

--- src/apps/App.js
+++ src/apps/App.js
@@ -1,3 +1,13 @@
+// container apps:
+
+// 1.clock-----alarm, time, stopwatch, timer
+
+// 2.task-----make notes,remainder,tasks etc.
+
+// 3.file identifier -----open file from local and accept perticular files.
+
+// 4.google map api --- show map from google.
+
 import React from "react";
 import TimerApp from "./TimerApp";
 import Container from "../styles/Container";
