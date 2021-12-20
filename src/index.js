1680a42dee340aea7f66c6842ddce6129c58d9b8
Mohit <mohitgadhavi1@gmail.com>
2021-12-20 15:04:04 +0530

    reg-commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import GlobalStyle from "./styles/GlobaleStyle";
+import { Provider } from "react-redux";
+import store from "./store/index";
+
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
